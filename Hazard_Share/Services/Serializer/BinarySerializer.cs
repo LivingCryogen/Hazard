687a206d75dd707c20c16676288274d035218b95
LivingCryogen <118312603+LivingCryogen@users.noreply.github.com>
2024-10-12 17:41:09 -0500

    Beginning new BinarySerializer documentation.

--- Hazard_Share/Services/Serializer/BinarySerializer.cs
+++ Hazard_Share/Services/Serializer/BinarySerializer.cs
@@ -4,16 +4,28 @@ using System.Text;
 
 namespace Hazard_Share.Services.Serializer;
 
+/// <summary>
+/// A binary serializer for <see cref="IBinarySerializable"/> objects. 
+/// </summary>
+/// <remarks>
+/// <para><see cref="BinarySerializer"/> and <see cref="IBinarySerializable"/> work with <see cref="SerializedData"/>, which requires <see cref="IConvertible"/> values.
+/// This serializer encodes <see cref="IConvertible"/> values, with un-encoded optional <see cref="string">tags</see>.</para>
+/// <para>To read back tagged values, (assuming BinaryReader reader):
+/// <example><code>var readTag = reader.readString();<br/>
+/// var readValue = (T)BinarySerializer.ReadConvertible(reader, typeof(T));</code><br/>
+/// While reading back untagged values requires only:
+/// <code>var readValue = (T)BinarySerializer.ReadConvertible(reader, typeof(T));</code></example></para>
+/// </remarks>
 public static class BinarySerializer
 {
     private static ILogger? _logger;
+    /// <summary>
+    /// Initializes this <see cref="BinarySerializer"/>'s <see cref="ILogger"/>.
+    /// </summary>
+    /// <param name="loggerFactory">An <see cref="ILoggerFactory"/> configured to create the appropriate <see cref="ILogger"/>.</param>
     public static void InitializeLogger(ILoggerFactory loggerFactory)
     {
         _logger = loggerFactory.CreateLogger(typeof(BinarySerializer));
-    }
-    public static void InitializeLogger(ILogger logger)
-    {
-        _logger = logger;
     }
 
     #region Encoding Methods
@@ -57,36 +69,57 @@ public static class BinarySerializer
         foreach (IConvertible value in values)
             WriteConvertible(writer, type, value);
     }
+    /// <summary>
+    /// Reads an <see cref="IConvertible"/> value previously written using <see cref="BinarySerializer"/>.
+    /// </summary>
+    /// <param name="reader">A <see cref="BinaryReader"/> whose <see cref="BinaryReader.BaseStream"/> was previously written to.</param>
+    /// <param name="type">The final<see cref="Type"/> expected.</param>
+    /// <returns>An <see cref="IConvertible"/> whose <see cref="Type"/> matches <paramref name="type"/>.</returns>
+    /// <remarks><paramref name="type"/> is compatible with any <see cref="IConvertible"/>.</remarks>
     public static IConvertible ReadConvertible(BinaryReader reader, Type type)
     {
         int length = reader.ReadInt32();
         byte[] bytes = reader.ReadBytes(length);
         return BytesToConvertible(type, bytes);
     }
-    public static object ReadEnum(BinaryReader reader, Type type)
+    private static object ReadEnum(BinaryReader reader, Type type)
     {
         int length = reader.ReadInt32();
         byte[] bytes = reader.ReadBytes(length);
         return BytesToEnumObject(type, bytes);
     }
+    /// <summary>
+    /// Reads an array of <see cref="IConvertible"/> values previously written using <see cref="BinarySerializer"/>.
+    /// </summary>
+    /// <param name="reader">A <see cref="BinaryReader"/> whose <see cref="BinaryReader.BaseStream"/> was previously written to.</param>
+    /// <param name="type">The underlying <see cref="Type"/> of the expected <see cref="Array"/>. Must be an <see cref="IConvertible"/> other than <see cref="string"/> and <see cref="Enum"/>.</param>
+    /// <param name="numValues">The <see cref="int">number</see> of values expected in the read <see cref="Array"/>.</param>
+    /// <returns>An <see cref="Array"/> of <see cref="IConvertible"/>s, exlcuding <see cref="string"/> and <see cref="Enum"/>, whose <see cref="Type"/> matches <paramref name="type"/>.</returns>
+    /// <remarks>
+    /// <see cref="Array"/>s of type <see cref="string"/> and <see cref="Enum"/> require <see cref="ReadStrings(BinaryReader, Type, int)"/> and <see cref="ReadEnums(BinaryReader, Type, int)"/>, respectively.
+    /// </remarks>
+    /// <exception cref="ArgumentException">.</exception>
     public static Array ReadConvertibles(BinaryReader reader, Type type, int numValues)
     {
-        if (!typeof(IConvertible).IsAssignableFrom(type))
-            throw new ArgumentException("ReadConvertibles accepts only types which implement IConvertible.", nameof(type));
+        if (!typeof(IConvertible).IsAssignableFrom(type) || type.IsEnum || type == typeof(string))
+            throw new ArgumentException("ReadConvertibles accepts only IConvertible types, excluding strings and Enums.", nameof(type));
 
         Array returnArray = Array.CreateInstance(type, numValues);
         for (int i = 0; i < numValues; i++)
             returnArray.SetValue(ReadConvertible(reader, type), i);
         return returnArray;
     }
-    public static Array ReadStrings(BinaryReader reader, Type type, int numValues)
+    /// <summary>
+    /// Reads an array of <see cref="string"/> values previously written using <see cref="BinarySerializer"/>.
+    /// </summary>
+    /// <param name="reader">A <see cref="BinaryReader"/> whose <see cref="BinaryReader.BaseStream"/> was previously written to.</param>
+    /// <param name="numValues"></param>
+    /// <returns>An <see cref="Array"/> of <see cref="string"/>s.</returns>
+    public static Array ReadStrings(BinaryReader reader, int numValues)
     {
-        if (!type.IsAssignableFrom(typeof(string)))
-            throw new ArgumentException("ReadConvertibles accepts only string types.", nameof(type));
-
-        Array returnArray = Array.CreateInstance(type, numValues);
+        Array returnArray = Array.CreateInstance(typeof(string), numValues);
         for (int i = 0; i < numValues; i++)
-            returnArray.SetValue((string)ReadConvertible(reader, type), i);
+            returnArray.SetValue((string)ReadConvertible(reader, typeof(string)), i);
         return returnArray;
     }
     public static Array ReadEnums(BinaryReader reader, Type type, int numValues)
