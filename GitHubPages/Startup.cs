9f47f47e76662747e09f2053ccaf2603502ccc2a
LivingCryogen <118312603+LivingCryogen@users.noreply.github.com>
2025-01-07 09:30:31 -0600

    Trying to get GitHubPages hosted properly

---
+++ GitHubPages/Startup.cs
@@ -0,0 +1,11 @@
+﻿namespace GitHubPages;
+
+public class Startup
+{
+    public void Configure(IApplicationBuilder app, IHostEnvironment env)
+    {
+        if (env.IsDevelopment()) 
+            app.UseDeveloperExceptionPage();
+        app.UseStaticFiles();
+    }
+}
