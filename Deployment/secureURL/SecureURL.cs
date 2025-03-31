827365b4a4e100a38174d7132df63377bd514b0d
LivingCryogen <118312603+LivingCryogen@users.noreply.github.com>
2025-03-31 14:30:55 -0500

    Revisions to azure function to match new build naming and make it easier to adjust later

--- Deployment/secureURL/SecureURL.cs
+++ Deployment/secureURL/SecureURL.cs
@@ -22,19 +22,30 @@ namespace SecureURL
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
+            string architecture = req.QueryString.Value switch {
+                string value when value.Contains("x64") => "x64/",
+                string value when value.Contains("arm64") => "ARM64/",
+                _ => string.Empty
+            };
+
             string? accountName = Environment.GetEnvironmentVariable("StorageAccountName");
             string? containerName = Environment.GetEnvironmentVariable("ContainerName");
-            string? blobName = Environment.GetEnvironmentVariable("BlobName");
             string? connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__JAMStorageConnectionString");
             string? storageKey = Environment.GetEnvironmentVariable("StorageKey");
+            string? blobPrefix = Environment.GetEnvironmentVariable("BlobPrefix");
+            string? blobExtension = Environment.GetEnvironmentVariable("BlobExtension");
 
             if (string.IsNullOrEmpty(accountName) ||
                 string.IsNullOrEmpty(containerName) ||
-                string.IsNullOrEmpty(blobName) ||
                 string.IsNullOrEmpty(connectionString) ||
-                string.IsNullOrEmpty(storageKey)) {
+                string.IsNullOrEmpty(storageKey) ||
+                string.IsNullOrEmpty(blobPrefix) ||
+                string.IsNullOrEmpty(blobExtension) ||
+                string.IsNullOrEmpty(architecture)) {
                 return new UnprocessableEntityResult();
             }
+
+            string blobName = $"{architecture.ToLower()}{blobPrefix}{architecture}{blobExtension}";
 
             try {
                 // Create Blob Client
