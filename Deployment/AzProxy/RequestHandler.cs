74aa83151044f82b66945037bad187a140f2f1c4
LivingCryogen <118312603+LivingCryogen@users.noreply.github.com>
2025-04-06 09:45:18 -0500

    Manual testing bypassing request validation

--- Deployment/AzProxy/RequestHandler.cs
+++ Deployment/AzProxy/RequestHandler.cs
@@ -18,30 +18,31 @@ public class RequestHandler(ILogger<RequestHandler> logger, IConfiguration confi
 
     public bool ValidateRequest(string iPaddress)
     {
-        if (!_banService.CacheInitialized) {
-            int timeOut = 10000;
-            int checkInterval = 100;
-            int timeSpent = 0;
+        // commented out strictly for manual testing
+        //if (!_banService.CacheInitialized) {
+        //    int timeOut = 10000;
+        //    int checkInterval = 100;
+        //    int timeSpent = 0;
 
-            while (!_banService.CacheInitialized) {
-                Thread.Sleep(checkInterval);
-                timeSpent += checkInterval;
+        //    while (!_banService.CacheInitialized) {
+        //        Thread.Sleep(checkInterval);
+        //        timeSpent += checkInterval;
 
-                if (timeSpent >  timeOut) {
-                    _logger.LogError("Cache initialization timed out.");
-                    return false;
-                }
-            }
-        }
+        //        if (timeSpent >  timeOut) {
+        //            _logger.LogError("Cache initialization timed out.");
+        //            return false;
+        //        }
+        //    }
+        //}
 
-        // Create a request entry, or update one using concurrent-safe method
-        var (LastReset, Count) = _requestCounters.AddOrUpdate(iPaddress,
-            _ => (DateTime.UtcNow, 1),  // all new entries get this value
-            (_, oldValue) => // old entries reset or updated
-                (DateTime.UtcNow - oldValue.LastReset > _requestReset) ? (DateTime.UtcNow, 1) : (oldValue.LastReset, oldValue.Count + 1));
+        //// Create a request entry, or update one using concurrent-safe method
+        //var (LastReset, Count) = _requestCounters.AddOrUpdate(iPaddress,
+        //    _ => (DateTime.UtcNow, 1),  // all new entries get this value
+        //    (_, oldValue) => // old entries reset or updated
+        //        (DateTime.UtcNow - oldValue.LastReset > _requestReset) ? (DateTime.UtcNow, 1) : (oldValue.LastReset, oldValue.Count + 1));
 
-        if (!_banService.Allow(iPaddress, Count))
-            return false;
+        //if (!_banService.Allow(iPaddress, Count))
+        //    return false;
 
         return true;
     }
