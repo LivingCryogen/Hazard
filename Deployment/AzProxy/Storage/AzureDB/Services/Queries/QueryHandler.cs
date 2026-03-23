8b04ab4048d9f11805668a587e7d65ce9987c2fe
Josh <joshua.a.mcknight@gmail.com>
2026-03-23 11:18:56 -0500

    xfer

--- Deployment/AzProxy/Storage/AzureDB/Services/Queries/QueryHandler.cs
+++ Deployment/AzProxy/Storage/AzureDB/Services/Queries/QueryHandler.cs
@@ -1,4 +1,6 @@
 ﻿using HazardBackend.Storage.AzureDB.Context;
+using HazardBackend.Storage.AzureDB.Entities;
+using System.Runtime.CompilerServices;
 using System.Text.Json.Nodes;
 using static System.Formats.Asn1.AsnWriter;
 
@@ -6,11 +8,56 @@ namespace HazardBackend.Storage.AzureDB.Services.Queries;
 
 public class QueryHandler(GameStatsDbContext dbContext)
 {
-    public async Task<QueryResult> HandleQueryAsync(string queryType, string[] queryParams)
+    private readonly HashSet<string> _leaderBoardPropertyNames = 
+        [ 
+            "Name",
+            "GamesStarted",
+            "GamesCompleted",
+            "GamesWon",
+            "FirstGameStarted",
+            "FirstGameCompleted",
+            "LastGameStarted",
+            "LastGameCompleted",
+            "TotalGamesDuration",
+            "AttacksWon",
+            "AttacksLost",
+            "AttacksTied",
+            "Conquests",
+            "Retreats",
+            "ForcedRetreats",
+            "AttackDiceRolled",
+            "DefenseDiceRolled",
+            "Moves",
+            "MaxAdvances",
+            "TradeIns",
+            "TotalOccupationBonus"
+        ];
+
+
+
+    public async Task<QueryResult> HandleQueryAsync(QueryType queryType, string[] queryParams)
     {
-        
+        switch (queryType)
+        {
+            case QueryType.Leaderboard: return await GetLeaderboard(dbContext, queryParams); 
+            case QueryType.PlayerStats: return await GetPlayerStats(queryParams);
+
+        }
     }
 
-            //    using var scope = _serviceProvider.CreateScope();
-            //var dbContext = scope.ServiceProvider.GetRequiredService<GameStatsDbContext>();
+    private async Task<QueryResult> GetLeaderboard(string[] queryParams)
+    {
+        if (queryParams.Length == 0)
+            return new QueryResult("Empty leaderboard query parameter; no data fetched.", null);
+
+        if (string.IsNullOrEmpty(queryParams[0]))
+            return new QueryResult("Empty leaderboard sort parameter; no data fetched.", null);
+
+        if (!_leaderBoardPropertyNames.Contains(queryParams[0]))
+            return new QueryResult($"Invalid leaderboard sort parameter '{queryParams[0]}'; no data fetched.", null);
+
+
+        string sortPropertyName = queryParams[0];
+        
+    }
 }
---
+++ Deployment/AzProxy/Storage/AzureDB/Services/Queries/QueryType.cs
@@ -0,0 +1,10 @@
+﻿namespace HazardBackend.Storage.AzureDB.Services.Queries;
+
+public enum QueryType : int
+{
+    None = 0,
+    Leaderboard = 1,
+    GameSession = 2,
+    PlayerStats = 3,
+    Action = 4
+}
